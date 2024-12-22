27fa699ffc39584616391ca661b7035f21e4067e
Sharan Chakradhar Madduri <sharanchakradhar@outlook.com>
2024-12-22 19:24:51 +1000

    deref

--- src/main.rs
+++ src/main.rs
@@ -76,3 +76,25 @@
 // }
 //----------------------------------------------------------------------------------------------------------------------
 
+// fn main() {
+//     let text = "Madduri Rohith ";
+//     let repetitions = 9_999_999;
+
+//     // Build the output once to avoid repetitive printing overhead
+//     let output = text.repeat(repetitions);
+
+//     // Print the entire string in one go
+//     println!("{}", output);
+// }
+//----------------------------------------------------------------------------------------------------------------------
+
+fn buy_apples(apple_count: &mut i32, apples_bought: i32) {
+    *apple_count += apples_bought;
+}
+
+fn main() {
+    let mut my_apple_count = 5; // Starting with 5 apples
+    let shopping_cart = &mut my_apple_count;
+    buy_apples(shopping_cart, 10); // Buying 10 more apples
+    println!("I had {} apples; now I have {}!", 5, my_apple_count);
+}
